f6e01e376aae443c6cb1eba8d052f87ad2e2f6ab
Kidddooo <mahatoaakriti3@gmail.com>
2026-09-05 23:59:27 +0545

    player

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SideBar from './Component/SideBar'
+import Player from './Component/Player'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
         <div className='h-[90%] flex'>
             <SideBar />
         </div>
+        <Player />
       
     </div>
   )
