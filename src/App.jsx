4c4d8c51ee1f9fabc4b80e7b5be769d1c9f7b297
Kidddooo <mahatoaakriti3@gmail.com>
2026-09-06 22:18:51 +0545

    Navbar is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SideBar from './Component/SideBar'
 import Player from './Component/Player'
+import Display from './Component/Display'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
         <div className='h-[90%] flex'>
             <SideBar />
+            <Display />
         </div>
         <Player />
       
--- src/Component/DisplayHome.jsx
+++ src/Component/DisplayHome.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const DisplayHome = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <div className='my-5 font-bold text-2xl'>
+        
+      </div>
+    </>
   )
 }
 
