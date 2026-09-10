2950cd5abc532a9bd6bc3986779f09f0bb8f983a
Kidddooo <mahatoaakriti3@gmail.com>
2026-09-10 15:21:31 +0545

    use Navigation is completed , Now functionality

--- src/Component/DisplayHome.jsx
+++ src/Component/DisplayHome.jsx
@@ -21,7 +21,7 @@ const DisplayHome = () => {
         <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
         <div className='flex overflow-auto'>
           {songsData.map((item,index)=>(
-            <SongItem name={item.name} desc={item.desc} id={item.id} image={item.image} />
+            <SongItem key={item} name={item.name} desc={item.desc} id={item.id} image={item.image} />
           ))}
         </div>
       </div>
