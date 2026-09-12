742fec25a7009225de6e4380f67a5d4b1a7e48ac
Kidddooo <mahatoaakriti3@gmail.com>
2026-09-12 13:04:45 +0545

    Play and Pause functionality is done

--- src/Context/PlayerContext.jsx
+++ src/Context/PlayerContext.jsx
@@ -1,47 +1,64 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
 
-const PlayerContextProvider = (props)=>{
+const PlayerContextProvider = (props) => {
     const audioRef = useRef();
     const seekBg = useRef();
     const seekBar = useRef();
 
 
-    const [track,setTrack] = useState(songsData[0]);
-    const [playStatus,setPlayStatus] = useState(false);
-    const [time,setTime] = useState({
-        currentTime:{
-            second:0,
-            minute:0
+    const [track, setTrack] = useState(songsData[0]);
+    const [playStatus, setPlayStatus] = useState(false);
+    const [time, setTime] = useState({
+        currentTime: {
+            second: 0,
+            minute: 0
         },
-        totalTime:{
-            second:0,
-            minute:0
+        totalTime: {
+            second: 0,
+            minute: 0
         }
     })
 
-    const play = ()=>{
+    const play = () => {
         audioRef.current.play();
         setPlayStatus(true);
     }
-    const pause =()=>{
+    const pause = () => {
         audioRef.current.pause();
         setPlayStatus(false);
     }
+    useEffect(() => {
+        setTimeout(() => {
+            audioRef.current.ontimeupdate = () => {
+                setTime({
+                    currentTime: {
+                        second: Math.floor(audioRef.current.currentTime % 60),
+                        minute: Math.floor(audioRef.current.currentTime / 60)
+                    },
+                    totalTime: {
+                        second: Math.floor(audioRef.current.duration % 60),
+                        minute: Math.floor(audioRef.current.duration / 60)
+                    }
+                })
+            }
+
+        }, 1000)
+    })
     const contextValue = {
         audioRef,
         seekBg,
         seekBar,
         track,
         setTrack,
-        playStatus,setPlayStatus,
-        time,setTime,
-        play,pause
+        playStatus, setPlayStatus,
+        time, setTime,
+        play, pause
 
     }
-    return(
+    return (
         <PlayerContext.Provider value={contextValue}>
             {props.children}
         </PlayerContext.Provider>
